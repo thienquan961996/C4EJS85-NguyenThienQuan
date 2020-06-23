a7e1d2786d4dd278f66871b998cd419abcd8ee81
nguyenthienquan <nguyenthienquan1996@gmail.com>
2020-06-23 16:58:10 +0700

    bai tap video

---
+++ bai tap video/index.js
@@ -0,0 +1,135 @@
+ // let item1 = 'Com gao lut';
+ // let item2 = 'Chao ga';
+ // let item3 = 'Bugger';
+ // let item4 = 'Khoai tay chien';
+
+ // let menu = [ ];
+ // console.log(menu);
+
+ // let menu = [ 'Chao ga'];
+//  console.log(menu);
+
+// let menu = ['Chao' , 'Com' , 'Bugger' , 'Khoai' ];
+// console.log(menu);
+
+
+// // // let newItem = prompt('Enter new Item');
+// // // menu.push(newItem);
+// // // console.log(menu);  // creat
+
+// // let i = 1 ;
+// // console.log(menu[i]);  //read
+
+// console.log(menu[1]);
+// menu[1] = 'Mien ngu sac';
+// console.log(menu[1]);
+// console.log(menu);  // update
+
+// menu.splice(0, 1);
+// console.log(menu);
+
+//  for ( let i=0; i < menu.length; i++)
+// {
+//     menu[i] = menu[i].toUpperCase();
+// }
+// console.log(menu);
+
+
+// if (n) {
+//     console.log('Is Number');
+// }
+// else {
+//      console.log('Not a Number') ;  
+// }
+
+// let n;
+// while (true)
+// {
+//     n = Number(prompt('Enter a Number'));
+//    if (n) {
+//         break;
+//    }
+// }
+// console.log(n);
+
+
+
+
+
+
+// console.log("bài tập");
+// console.log("khởi tạo một mảng chứa tên những bộ phim bạn thích");
+
+// let phim = [ 'F&F' , 'one piece' , 'naruto' , 'JAV', 'Mắt biếc'];
+// console.log(phim); // khởi tạo mảng
+
+// // let newMovie = prompt('Enter new movie');
+// // phim.push(newMovie);
+// // console.log(phim);   // thêm mục mới
+
+// let i = prompt('Nhap vao vi tri can xem');
+// console.log(phim[i]);   // đọc thư mục ở vị trí i
+
+
+// let movieTitle = prompt('nhap vao ten phim');
+// phim[0] = movieTitle;
+// console.log(phim[0]);
+// console.log(phim);   // cập nhật mục đầu tiên
+
+// let movieTitle =prompt('nhap vao ten phim');
+// i = prompt('nhap vao vi tri');
+// phim[i] = movieTitle;
+// console.log(phim[i]);
+// console.log(phim);   // cập nhật mục ở vị trí i
+
+// let i = prompt('nhap vao vi tri can xoa');
+// phim.splice(i, 1);
+// console.log(phim);   // xóa 1 mục ở vị trí i
+
+// let i = prompt('nhap vao vi tri can xoa');
+// let n = prompt('nhap vao so phan tu');
+// phim.splice(i , n);
+// console.log(phim);   // xóa n mục ở vị trí i
+
+
+// for ( let i = 0; i < phim.length; i++)
+// {
+//     console.log(phim[i]);
+//  }                   // đọc tất cả mục trong mảng
+
+//  for ( let i = 0; i < phim.length/2; i++)
+//  {
+//      console.log(phim[i]);
+//   }                  // đọc nửa mục đầu tiên trong mảng
+
+// let n =prompt('nhap vao vi tri ');
+
+// for ( let i = 0; i < n; i++)
+//  {
+//       console.log(phim[i]);
+//   }                 // đọc tất cả các mục đến vị trí n
+
+//  for ( let i = 0; i < phim.length; i++)
+//  {
+//      phim[i] = phim[i].toLowerCase();
+//   } 
+//   console.log(phim);   // thay đổi chữ của mảng thành chữ thường
+
+// let n;
+// while (true) {
+//     n = prompt('nhap vao ten cua ban');
+//     if (n.length>15) {
+//        alert('ten cua ban qua dai');
+//     }else {
+//         alert('good name');
+//         break;
+//     }
+// }                    // nhập tên người dùng rồi kiểm tra tên có dài hay không
+
+
+// alert( ' Con nhện có bao nhiêu chân : \n\
+// 1. không có \n\
+// 2. 4 chân \n\
+// 3. 8 chân \n\
+// 4. 12 chân' )
+
