a8b48ea90dacdf4a81ff8d93883e0d145c3b4a1b
nguyenthienquan <nguyenthienquan1996@gmail.com>
2020-08-10 14:30:48 +0700

    test

---
+++ NguyenThienQuan-TestCI-0393564079/index.js
@@ -0,0 +1,39 @@
+// console.log('Bai1');
+
+// let n = Number(prompt('Nhập số phần tử của dãy'));
+// let firstnum = Number(prompt('Nhập số đầu tiên của dãy'));
+
+// if (n % 2 == 0 && n > 1) {
+//     console.log('số đối diện với firstNum là :' + Number(firstnum + (n / 2)));
+// } else {
+//     console.log('Không có số đối diện');
+// };
+
+// console.log('Bai2');
+
+// let firstString = prompt("Enter first string:");
+// let secondString = prompt("Enter second string:");
+// console.log(firstString + " " + secondString);
+// let splitFirstString = firstString.split("");
+// console.log(splitFirstString);
+// let splitSecondString = secondString.split("");
+// console.log(splitSecondString);
+// let array = [];
+// let stringLength;
+// if (splitFirstString.length >= splitSecondString.length) {
+//     stringLength = splitFirstString;
+// } else {
+//     stringLength = splitSecondString
+// }
+// for (let i = 0; i < stringLength.length; i++) {
+//     if (splitFirstString.length <= i) {
+//         array.push(splitSecondString[i]);
+//     } else if (splitSecondString.length <= i) {
+//         array.push(splitFirstString[i]);
+//     } else {
+//         array.push(splitFirstString[i]);
+//         array.push(splitSecondString[i]);
+//     }
+// }
+// console.log(array.join(''));
+
